b2859b03465a215d5921d5d3161da97317cb2a41
A-Deepak1610 <deepaka.ad24@bitsathy.ac.in>
2024-11-09 14:46:57 +0530

    Final

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 function main(){
     var date=document.getElementById("date").value;
+    if(date==""){
+        alert("Please Choose Date")
+    }
+    else{
     var output=document.getElementById("out");
     const cyear=new Date().getFullYear();
     console.log(cyear);
     const year = new Date(date).getFullYear();
     console.log(year);
     var age=cyear-year;
-    output.textContent=`Age:${age}`;
-    
+    output.textContent=`You are ${age} years old!`;
+    }  
 }
